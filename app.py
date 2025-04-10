56812630c4d1c04042d5153f739501d54c13e113
Wasif Mehmood <wasif.mehmood@visionx.io>
2025-04-10 12:49:07 +0500

    commit

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ if not GROQ_API_KEY:
 # Set environment variables
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+os.environ["CHROMA_DB_IMPL"] = "duckdb"
 
 st.markdown("""
 <style>
